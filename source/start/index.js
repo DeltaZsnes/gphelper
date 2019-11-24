080c8af02e38ad04e5b0326b60510ffaec03dd15
Daniel Zheng <daniel.zheng@barnebys.com>
2019-11-25 00:30:28 +0900

    added video

--- source/start/index.js
+++ source/start/index.js
@@ -1,24 +1,34 @@
 console.log("index.js");
 
 const loginRedirect = async () => {
-    window.location.replace("/auth/google");
+    window.location.replace("/auth/login");
 };
 
 const runAsync = async () => {
-    var authUserResponse = await fetch("/auth/user", {
+    const authUserResponse = await fetch("/auth/user", {
         "body": null,
         "method": "GET",
         "mode": "cors"
     });
     console.log(authUserResponse);
-    
-    if (authUserResponse.status == 200){
+
+    if (authUserResponse.status == 200) {
         console.log(await authUserResponse.json());
     }
 
-    if (authUserResponse.status == 401){
+    if (authUserResponse.status == 401) {
         await loginRedirect();
     }
+    
+    await captureVideo();
+};
+
+const captureVideo = async () => {
+    const video = document.querySelector('video');
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: true
+    });
+    video.srcObject = stream;
 };
 
 runAsync().catch(e => {
